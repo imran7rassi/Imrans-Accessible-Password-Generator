e82fae388b3a2aca0b1c50230324b0c17d80300c
imran-rassi <imran.rassi.2020@gmail.com>
2022-09-15 20:07:34 -0400

    added functions to Script

--- script.js
+++ script.js
@@ -4,6 +4,24 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  var userInput = window.prompt("How long do you want your password to be?")
+
+  var passordLenght = parseInt(userInput)
+
+  if (isNaN(passordLenght)) {
+    window.alert("That's not a number!")
+    return
+  }
+
+  if (passordLenght < 8 || passordLenght > 128) {
+    window.alert("Password lenght must be between 8 and 128 characters")
+    return
+  }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
